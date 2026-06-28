c2bd7b921fd56c8f21b448c40a5ec1cd2ed1a17f
rishu-29x <rishu29x@gmail.com>
2026-06-29 02:19:41 +0530

    feat: basic LED blink at 500ms interval v1.0
    Removed blink counter and simplified the LED blinking logic.

--- IoT Week1/LED Blink/code.ino
+++ IoT Week1/LED Blink/code.ino
@@ -1,28 +1,12 @@
-// LED Blink Program - v1.3
-// Description: Added blink counter and improved structure
-
-const int LED_PIN = 13;
-const int BLINK_DELAY = 1000;
-
-int blinkCount = 0;
+int ledPin = 13;
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(9600);
-  Serial.println("LED Blink Program v1.3 Started");
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  blinkCount++;
-
-  digitalWrite(LED_PIN, HIGH);
-  Serial.print("Blink #");
-  Serial.print(blinkCount);
-  Serial.println(" - LED ON");
-  delay(BLINK_DELAY);
-
-  digitalWrite(LED_PIN, LOW);
-  Serial.println("LED OFF");
-  //sensor logging placeholder - issue #1
-  delay(BLINK_DELAY);
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
 }
