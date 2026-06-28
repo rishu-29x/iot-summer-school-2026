aa6d5f3f80afa6b71a6a8bbf6d7b51eb901d33a2
rishu-29x <rishu29x@gmail.com>
2026-06-29 01:28:29 +0530

    feat: add named constants and comments to LED blink v1.1
    Updated LED blink program to use constants for pin and delay.

--- IoT Week1/LED Blink/code.ino
+++ IoT Week1/LED Blink/code.ino
@@ -1,12 +1,16 @@
-int ledPin = 13;
+// LED Blink Program - v1.1
+// Description: Basic LED blink with named constants and comments
+
+const int LED_PIN = 13;      // Built-in LED pin on Arduino Uno
+const int BLINK_DELAY = 1000; // Delay in milliseconds
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);  // Set LED pin as output
 }
-void loop(){
-  digitalWrite(ledPin,HIGH);
-  delay(1000);
-  digitalWrite(ledPin,LOW);
-  delay(1000);
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);  // Turn LED ON
+  delay(BLINK_DELAY);
+  digitalWrite(LED_PIN, LOW);   // Turn LED OFF
+  delay(BLINK_DELAY);
 }
-  
