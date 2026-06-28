2889af53a732e69339acc9d172bed62ff4502800
rishu-29x <rishu29x@gmail.com>
2026-06-29 02:34:29 +0530

    feat: basic LED blink at 500ms interval v1.0
    Updated LED blink timing from 1 second to 500 milliseconds.

--- IoT Week1/LED Blink/LED BLINK code.ino
+++ IoT Week1/LED Blink/LED BLINK code.ino
@@ -1,13 +1,10 @@
 void setup() {
-pinMode (13, OUTPUT) ;
-
-1
+  pinMode(13, OUTPUT);
+}
 
 void loop() {
-
-digitalWrite (13, HIGH);
-
-delay(1000);
-digitalWrite (13, LOW);
-delay(1000);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
 }
