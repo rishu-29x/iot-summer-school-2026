13c66c2403e1569f80f41a2a522b0267cf09cce3
rishu-29x <rishu29x@gmail.com>
2026-06-29 02:35:14 +0530

    feat: add serial print blink count v1.1
    Added serial output to display blink count.

--- IoT Week1/LED Blink/LED BLINK code.ino
+++ IoT Week1/LED Blink/LED BLINK code.ino
@@ -1,10 +1,16 @@
+int blinkCount = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  blinkCount++;
   digitalWrite(13, HIGH);
   delay(500);
   digitalWrite(13, LOW);
   delay(500);
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
